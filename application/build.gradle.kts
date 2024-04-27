edbd9ff0e30f31dc6a27f86f05e53936b4fe8f8c
IliaSuponeff <suponeff.i@gmail.com>
2024-04-27 13:34:02 +0300

    feat: add application subproject
    Subproject implements 'Desktop Compose' library and contains template code of application

---
+++ application/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+    id("org.jetbrains.compose") version "1.5.10"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,20 +6,15 @@
  * This project uses @Incubating APIs which are subject to change.
  */
 plugins {
-	kotlin("jvm") version "1.9.23"
+	kotlin("jvm") version "1.9.20"
 }
 
 repositories {
 	mavenCentral()
+	maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+	google()
 }
 
-dependencies {
-	testImplementation("org.jetbrains.kotlin:kotlin-test")
-}
-
-tasks.test {
-	useJUnitPlatform()
-}
 kotlin {
-	jvmToolchain(17)
+	jvmToolchain(21)
 }
