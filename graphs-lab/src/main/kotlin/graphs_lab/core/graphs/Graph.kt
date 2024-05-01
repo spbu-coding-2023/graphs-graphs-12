a765a9327f31d0876eb30f779eaa46f79e64c54e
IliaSuponeff <suponeff.i@gmail.com>
2024-05-01 15:59:31 +0300

    docs: add documentation at KDocs format for all classes at 'graphs-lab.core' package
    Refactor Vertex class by removing 'open' modifier. Refactor WeightedEdge class by removing not valid method: 'equals', 'hashCode'.

--- graphs-lab/src/main/kotlin/graphs_lab/core/graphs/Graph.kt
+++ graphs-lab/src/main/kotlin/graphs_lab/core/graphs/Graph.kt
@@ -4,6 +4,15 @@ import graphs_lab.core.edges.Edge
 import graphs_lab.core.Vertex
 
 
+/**
+ * Provides an abstract representation of a graph structure.
+ *
+ * @param I the type of the vertex identifiers
+ * @param E the type of the edges in the graph
+ * @property label the label or name of the graph
+ * @property isDirected indicates whether the graph is directed or not
+ * @property isAutoAddVertex specifies whether new vertices should be automatically added when adding edges
+ */
 abstract class Graph<I, E : Edge<I>>(
 	val label: String,
 	val isDirected: Boolean = false,
@@ -12,20 +21,42 @@ abstract class Graph<I, E : Edge<I>>(
 	private val vertices = mutableMapOf<I, Vertex<I>>()
 	private val edges = mutableMapOf<I, MutableSet<E>>()
 
+	/**
+	 * Adds a vertex to the graph with the specified id.
+	 *
+	 * @param id the identifier of the vertex to add
+	 */
 	fun addVertex(id: I) {
 		vertices.putIfAbsent(id, Vertex(id))
 	}
 
+	/**
+	 * Removes the vertex with the specified id from the graph.
+	 *
+	 * @param id the identifier of the vertex to remove
+	 */
 	fun removeVertex(id: I) {
 		vertices.remove(id)
 		clearEdgesVertex(id)
 	}
 
+	/**
+	 * Removes the edge between the vertices with the specified ids from the graph.
+	 *
+	 * @param idSource the identifier of the source vertex
+	 * @param idTarget the identifier of the target vertex
+	 */
 	fun removeEdge(idSource: I, idTarget: I) {
 		removeDirectedEdge(idSource, idTarget)
 		if (!isDirected) removeDirectedEdge(idTarget, idSource)
 	}
 
+	/**
+	 * Checks if the graph contains a vertex with the specified id.
+	 *
+	 * @param id the identifier of the vertex to check
+	 * @return true if the graph contains the specified vertex, false otherwise
+	 */
 	fun containsVertex(id: I): Boolean {
 		return vertices[id] != null
 	}
@@ -37,6 +68,13 @@ abstract class Graph<I, E : Edge<I>>(
 		return builder.append(")").toString()
 	}
 
+	/**
+	 * Adds the specified edge to the graph.
+	 * If graph is directed add edge from _edge.idTarget_ to _edge.idSource_
+	 *
+	 * @param edge the edge to add to the graph
+	 * @throws AssertionError if graph is not at auto add vertex mode and one of edge's vertex is not in graph
+	 */
 	protected fun addEdge(edge: E) {
 		assertContainsVertex(edge.idSource)
 		assertContainsVertex(edge.idTarget)
@@ -44,6 +82,12 @@ abstract class Graph<I, E : Edge<I>>(
 		if (!isDirected) updateEdgesContainer(reverseEdge(edge))
 	}
 
+	/**
+	 * Removes the edge between the vertices with the specified ids from the graph.
+	 *
+	 * @param idSource the identifier of the source vertex
+	 * @param idTarget the identifier of the target vertex
+	 */
 	private fun removeDirectedEdge(idSource: I, idTarget: I) {
 		val setEdgesVertex = edges[idSource] ?: return
 		for (edge in setEdgesVertex) {
@@ -55,17 +99,34 @@ abstract class Graph<I, E : Edge<I>>(
 		if (setEdgesVertex.isEmpty()) edges.remove(idSource)
 	}
 
+	/**
+	 * Removes all edges connected to the specified vertex from the graph.
+	 *
+	 * @param id the identifier of the vertex to clear edges for
+	 */
 	private fun clearEdgesVertex(id: I) {
 		for (idVertex in vertices.keys) removeDirectedEdge(idVertex, id)
 		edges.remove(id)
 	}
 
+	/**
+	 * Updates the container of edges connected to the specified vertex in the graph.
+	 *
+	 * @param edge the edge to add to the container of edges connected to the specified vertex
+	 */
 	private fun updateEdgesContainer(edge: E) {
 		val setEdgesVertex = edges.getOrDefault(edge.idSource, mutableSetOf())
 		setEdgesVertex.add(edge)
 		edges.putIfAbsent(edge.idSource, setEdgesVertex)
 	}
 
+	/**
+	 * Assertion is vertex with [id] contains in graph.
+	 * If graph is at auto add vertex mode than add new vertex with [id].
+	 *
+	 * @param id the vertex's id
+	 * @throws AssertionError if graph is not at auto add vertex mode and vertex with [id] not contains in graph
+	 */
 	private fun assertContainsVertex(id: I) {
 		if (!isAutoAddVertex && !containsVertex(id)) throw AssertionError(
 			"Graph $label is not auto added. Trying add edge with vertex $id that is not in it"
@@ -73,6 +134,11 @@ abstract class Graph<I, E : Edge<I>>(
 		else addVertex(id)
 	}
 
+	/**
+	 * Converts the set of edges into a string representation.
+	 *
+	 * @return a string representation of the set of edges connected to a vertex.
+	 */
 	private fun edgesToString(): String {
 		val viewEdges = mutableSetOf<String>()
 		for (idVertex in edges.keys) {
@@ -83,8 +149,20 @@ abstract class Graph<I, E : Edge<I>>(
 		return viewEdges.toString()
 	}
 
+	/**
+	 * Converts the edge into a string representation.
+	 *
+	 * @param edge the weighted edge to convert
+	 * @return a string representing the edge
+	 */
 	protected abstract fun edgeToString(edge: E): String
 
+	/**
+	 * Reverses the direction of the specified [edge].
+	 *
+	 * @param edge the edge to reverse
+	 * @return the edge with its source and target vertices swapped
+	 */
 	protected abstract fun reverseEdge(edge: E): E
 
 }
--- graphs-lab/src/main/kotlin/graphs_lab/core/graphs/UnweightedGraph.kt
+++ graphs-lab/src/main/kotlin/graphs_lab/core/graphs/UnweightedGraph.kt
@@ -3,12 +3,26 @@ package graphs_lab.core.graphs
 import graphs_lab.core.edges.Edge
 
 
+/**
+ * Represents an unweighted graph that extends the Graph class.
+ *
+ * @param I the type of the vertex identifiers
+ * @property id the identifier or name of the graph
+ * @property isDirected indicates whether the graph is directed or not
+ * @property isAutoAddVertex specifies whether new vertices should be automatically added when adding edges
+ */
 class UnweightedGraph<I>(
 	id: String,
 	isDirected: Boolean = false,
 	isAutoAddVertex: Boolean = false
 ) : Graph<I, Edge<I>>(id, isDirected, isAutoAddVertex) {
 
+	/**
+	 * Adds an unweighted edge between the vertices with the specified ids to the graph.
+	 *
+	 * @param idSource the identifier of the source vertex
+	 * @param idTarget the identifier of the target vertex
+	 */
 	fun addEdge(idSource: I, idTarget: I) = addEdge(
 		Edge(idSource, idTarget)
 	)
