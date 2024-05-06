47bb24cfe4f3def68b57a0ad8bb65a4afea64c18
IliaSuponeff <suponeff.i@gmail.com>
2024-05-06 19:51:50 +0300

    feat: add search graph's cycles algs

---
+++ graphs-lab/src/main/kotlin/graphs_lab/algs/CyclesSearchAlgorithms.kt
@@ -0,0 +1,8 @@
+package graphs_lab.algs
+
+import graphs_lab.core.edges.Edge
+import graphs_lab.core.graphs.Graph
+
+fun <I, E : Edge<I>> searchCyclesInDirectedGraph(graph: Graph<I, E>) {
+//	if (!graph.isDirected) throw
+}
