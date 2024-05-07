f7fc578371d27b72607eb619de0f8abba428b142
IliaSuponeff <suponeff.i@gmail.com>
2024-05-08 00:04:22 +0300

    refactor: change place 'detekt' gradle plugin implementation

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,30 +1,25 @@
 plugins {
-    kotlin("jvm") version "1.9.20"
-    id("org.jetbrains.compose") version "1.5.10"
+	kotlin("jvm") version "1.9.20"
+	id("org.jetbrains.compose") version "1.5.10"
 }
 
 repositories {
-    mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    google()
+	mavenCentral()
+	maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+	google()
 }
 
-
 dependencies {
-    implementation(compose.desktop.currentOs)
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.3")
+	implementation(compose.desktop.currentOs)
 }
 
 compose.desktop {
-    application {
-        mainClass = "MainKt"
-    }
+	application {
+		mainClass = "MainKt"
+	}
 }
 
 kotlin {
-    jvmToolchain(17)
+	jvmToolchain(17)
 }
 
-detekt {
-    config.setFrom("../scripts/detekt-config.yml")
-}
