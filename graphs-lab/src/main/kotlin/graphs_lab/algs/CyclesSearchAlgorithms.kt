d342a7505b46a7fae861f20f5afdd4c767f10b30
IliaSuponeff <suponeff.i@gmail.com>
2024-05-08 21:22:23 +0300

    docs: add KDocs for CyclesSearchAlgorithms

--- graphs-lab/src/main/kotlin/graphs_lab/algs/CyclesSearchAlgorithms.kt
+++ graphs-lab/src/main/kotlin/graphs_lab/algs/CyclesSearchAlgorithms.kt
@@ -3,10 +3,23 @@ package graphs_lab.algs
 import graphs_lab.core.edges.Edge
 import graphs_lab.core.graphs.Graph
 
+/**
+ * Class for searching cycles in graph.
+ *
+ * @param I The type of the vertex ID
+ * @param E The type of the edge
+ * @property graph The graph to search for cycles in
+ */
 class CyclesSearchAlgorithms<I, E : Edge<I>>(val graph: Graph<I, E>) {
 	private val cyclesPaths = mutableSetOf<List<I>>()
 	private val visitedVertices = mutableSetOf<I>()
 
+	/**
+	 * Searches for cycles containing the specified vertex in the graph.
+	 *
+	 * @param idVertex The ID of the vertex to search cycles for
+	 * @return A set of lists representing cycles containing the specified vertex
+	 */
 	fun searchVertexCycles(idVertex: I): Set<List<I>> {
 		cyclesPaths.clear()
 		findCycles(
@@ -17,6 +30,13 @@ class CyclesSearchAlgorithms<I, E : Edge<I>>(val graph: Graph<I, E>) {
 		return cyclesPaths.toSet()
 	}
 
+	/**
+	 * Recursively finds cycles containing the specified vertex.
+	 *
+	 * @param startVertex The starting vertex for the cycle search
+	 * @param currentVertex The current vertex during traversal
+	 * @param path The current path through the graph
+	 */
 	private fun findCycles(startVertex: I, currentVertex: I, path: MutableList<I>) {
 		if (path.size > 2 && currentVertex == startVertex) cyclesPaths.add(path.toList())
 		if (currentVertex in visitedVertices) return
