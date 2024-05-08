291ce613f20a83b1ba03eefb60d3b0a949899844
Maxim Rodionov <rodionov.maxim05@gmail.com>
2024-05-09 02:29:33 +0300

    test: add new test to TestTarjanStrongConnectivityInspector

--- graphs-lab/src/test/kotlin/graphs_lab/algs/TestTarjanStrongConnectivityInspector.kt
+++ graphs-lab/src/test/kotlin/graphs_lab/algs/TestTarjanStrongConnectivityInspector.kt
@@ -57,4 +57,48 @@ class TestTarjanStrongConnectivityInspector {
 			inspector.stronglyConnectedComponents()
 		)
 	}
+
+	@Test
+	@DisplayName("search for strongly connected components 2")
+	fun findingStronglyConnectedComponents2() {
+		/**
+		 * Image of a graph.
+		 *     7             11     --→     5
+		 *   ↗   ↘         ↙   ↘          ↙   ↘
+		 * 0   ↓   1  →  9   ←   2  →  10   ↑   3
+		 *   ↘   ↗         ↘   ↗          ↘   ↙
+		 *     6     --→     4              8
+		 */
+		val graph = UnweightedGraph<String>("3", isDirected = true, isAutoAddVertex = true)
+		graph.addEdge("0", "7")
+		graph.addEdge("6", "0")
+		graph.addEdge("7", "6")
+		graph.addEdge("7", "1")
+		graph.addEdge("6", "1")
+		graph.addEdge("6", "4")
+		graph.addEdge("1", "9")
+		graph.addEdge("9", "4")
+		graph.addEdge("4", "2")
+		graph.addEdge("2", "9")
+		graph.addEdge("11", "9")
+		graph.addEdge("11", "2")
+		graph.addEdge("11", "5")
+		graph.addEdge("2", "10")
+		graph.addEdge("5", "10")
+		graph.addEdge("10", "8")
+		graph.addEdge("5", "3")
+		graph.addEdge("8", "5")
+		graph.addEdge("3", "8")
+		val inspector = TarjanStrongConnectivityInspector(graph)
+		Assertions.assertEquals(
+			mapOf(
+				0 to setOf("3", "5", "8", "10"),
+				1 to setOf("2", "4", "9"),
+				2 to setOf("1"),
+				3 to setOf("6", "7", "0"),
+				4 to setOf("11")
+			),
+			inspector.stronglyConnectedComponents()
+		)
+	}
 }
