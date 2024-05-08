655262edc7c753eb3658aae7b5e65847dc48f0ca
IliaSuponeff <suponeff.i@gmail.com>
2024-05-08 21:13:14 +0300

    feat: add finding cycles alg for selected vertex

--- graphs-lab/src/main/kotlin/graphs_lab/algs/CyclesSearchAlgorithms.kt
+++ graphs-lab/src/main/kotlin/graphs_lab/algs/CyclesSearchAlgorithms.kt
@@ -3,6 +3,35 @@ package graphs_lab.algs
 import graphs_lab.core.edges.Edge
 import graphs_lab.core.graphs.Graph
 
-fun <I, E : Edge<I>> searchCyclesInDirectedGraph(graph: Graph<I, E>) {
-//	if (!graph.isDirected) throw
+class CyclesSearchAlgorithms<I, E : Edge<I>>(val graph: Graph<I, E>) {
+	private val cyclesPaths = mutableSetOf<List<I>>()
+	private val visitedVertices = mutableSetOf<I>()
+
+	fun searchVertexCycles(idVertex: I): Set<List<I>> {
+		cyclesPaths.clear()
+		findCycles(
+			startVertex = idVertex,
+			currentVertex = idVertex,
+			path = mutableListOf()
+		)
+		return cyclesPaths.toSet()
+	}
+
+	private fun findCycles(startVertex: I, currentVertex: I, path: MutableList<I>) {
+		if (path.size > 2 && currentVertex == startVertex) cyclesPaths.add(path.toList())
+		if (currentVertex in visitedVertices) return
+
+		path.add(currentVertex)
+		visitedVertices.add(currentVertex)
+		for (edge in graph.vertexEdges(currentVertex)) {
+			findCycles(
+				startVertex = startVertex,
+				currentVertex = edge.idTarget,
+				path = path
+			)
+		}
+
+		visitedVertices.remove(currentVertex)
+		path.removeLast()
+	}
 }
