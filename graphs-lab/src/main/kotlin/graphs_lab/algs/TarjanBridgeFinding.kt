f56407b7d0c2f2a23a464aafcdfb796c9202c0ed
Friend-zva <friend.zva@yandex.ru>
2024-05-09 22:31:56 +0300

    feat: implement the Tarjan bridge finding algorithm

---
+++ graphs-lab/src/main/kotlin/graphs_lab/algs/TarjanBridgeFinding.kt
@@ -0,0 +1,61 @@
+package graphs_lab.algs
+
+import graphs_lab.algs.utils.removeAndReturn
+import graphs_lab.core.edges.Edge
+import graphs_lab.core.graphs.Graph
+
+class TarjanBridgeFinding<I>(val graph: Graph<I, Edge<I>>) {
+	private val table = mutableSetOf<Edge<I>>()
+	private val tableHeights = mutableMapOf<I, Pair<Int?, Int?>>()
+	private val heapUnvisited = mutableListOf<I>()
+
+	init {
+		require(!graph.isDirected) { "Do not support directed graph." }
+	}
+
+	fun getBridges(): MutableSet<Edge<I>> {
+		if (graph.idVertices.isEmpty()) return table
+
+		graph.idVertices.forEach { heapUnvisited.add(it) }
+
+		while (heapUnvisited.isNotEmpty()) {
+			dfs(graph.vertexEdges(heapUnvisited.removeFirst()), 1)
+		}
+
+		return table
+	}
+
+	private fun dfs(setEdges: Set<Edge<I>>, height: Int) {
+		if (setEdges.isEmpty()) return
+
+		tableHeights[setEdges.first().idSource] = Pair(height, height)
+
+		setEdges.forEach {
+			var temp: Int
+
+			if (tableHeights[it.idTarget] == null) {
+				dfs(graph.vertexEdges(heapUnvisited.removeAndReturn(it.idTarget)), height + 1)
+				temp = checkAndGetFirst(tableHeights[it.idTarget])
+			} else {
+				temp = checkAndGetSecond(tableHeights[it.idTarget])
+				if (height - temp == 1) {
+					temp = height
+				}
+			}
+
+			tableHeights[it.idSource] = Pair(minOf(temp, checkAndGetFirst(tableHeights[it.idSource])), height)
+
+			if (checkAndGetFirst(tableHeights[it.idTarget]) > height) {
+				table.add(it)
+			}
+		}
+	}
+
+	private fun checkAndGetFirst(pair: Pair<Int?, Int?>?): Int {
+		return pair?.first ?: throw ExceptionInInitializerError("Undefined behaviour: unfounded vertex.")
+	}
+
+	private fun checkAndGetSecond(pair: Pair<Int?, Int?>?): Int {
+		return pair?.second ?: throw ExceptionInInitializerError("Undefined behaviour: unfounded vertex.")
+	}
+}
