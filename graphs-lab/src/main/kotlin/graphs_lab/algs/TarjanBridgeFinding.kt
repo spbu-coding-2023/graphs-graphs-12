659ded0fa7b838d3c45dc51a9893681e28df1310
Friend-zva <friend.zva@yandex.ru>
2024-05-12 00:15:12 +0300

    docs: add doc-stings for the Tarjan algo

--- graphs-lab/src/main/kotlin/graphs_lab/algs/TarjanBridgeFinding.kt
+++ graphs-lab/src/main/kotlin/graphs_lab/algs/TarjanBridgeFinding.kt
@@ -6,31 +6,59 @@ import graphs_lab.algs.utils.removeAndReturn
 import graphs_lab.core.edges.Edge
 import graphs_lab.core.graphs.Graph
 
+/**
+ * The Tarjan's bridge-finding algorithm.
+ *
+ * Finding the bridges in an undirected graph.
+ * A bridge is an edge, when it is removed, the number of connectivity components increases.
+ *
+ * @references https://en.wikipedia.org/wiki/Bridge_(graph_theory)
+ *
+ * @param I the type of the vertex identifiers
+ * @param E the type of the edges in the graph
+ * @property graph the input graph
+ */
 class TarjanBridgeFinding<I, E : Edge<I>>(
 	val graph: Graph<I, E>
 ) {
-	private val table = mutableSetOf<Edge<I>>()
+	private val setBridges = mutableSetOf<Edge<I>>()
+
+	/**
+	 * [tableHeights] the map that associates the identifier of a vertex with a pair that consist of
+	 * the minimal height (in an imaginary tree) among the vertices connected by an inverse edge (in an imaginary tree)
+	 * to the vertices from the subtree with the associated vertex in root or the height (in an imaginary tree)
+	 * of the associated vertex and the height (in an imaginary tree) of the associated vertex.
+	 */
 	private val tableHeights = mutableMapOf<I, Pair<Int?, Int?>>()
-	private val listUnvisited = mutableListOf<I>()
+	private val listUnvisitedVertices = mutableListOf<I>()
 
 	init {
 		require(!graph.isDirected) { "Do not support directed graph." }
 	}
 
+	/**
+	 * @return [setBridges]
+	 * @throws IllegalStateException if there is an appeal to a non-existent identifier
+	 */
 	fun getBridges(): Set<Edge<I>> {
-		if (graph.idVertices.isEmpty()) return table
+		if (graph.idVertices.isEmpty()) return setBridges
 
 		graph.idVertices.forEach {
-			listUnvisited.add(it)
+			listUnvisitedVertices.add(it)
 		}
 
-		while (listUnvisited.isNotEmpty()) {
-			dfs(graph.vertexEdges(listUnvisited.removeFirst()), 1)
+		while (listUnvisitedVertices.isNotEmpty()) {
+			dfs(graph.vertexEdges(listUnvisitedVertices.removeFirst()), 1)
 		}
 
-		return table
+		return setBridges
 	}
 
+	/**
+	 * Goes down the tree to the depth and fills [tableHeights] in according to the definition.
+	 *
+	 * @throws IllegalStateException if there is an appeal to a non-existent identifier
+	 */
 	private fun dfs(setEdges: Set<Edge<I>>, height: Int) {
 		if (setEdges.isEmpty()) return
 
@@ -39,12 +67,12 @@ class TarjanBridgeFinding<I, E : Edge<I>>(
 		setEdges.forEach {
 			var temp: Int
 
-			if (tableHeights[it.idTarget] == null) {
-				dfs(graph.vertexEdges(listUnvisited.removeAndReturn(it.idTarget)), height + 1)
+			if (tableHeights[it.idTarget] == null) { // Means a straight edge (in an imaginary tree)
+				dfs(graph.vertexEdges(listUnvisitedVertices.removeAndReturn(it.idTarget)), height + 1)
 				temp = checkAndGetFirst(tableHeights[it.idTarget])
-			} else {
+			} else { // Means an inverse edge (in an imaginary tree)
 				temp = checkAndGetSecond(tableHeights[it.idTarget])
-				if (height - temp == 1) {
+				if (height - temp == 1) { // Avoids considering an inverse edge whose vertex heights differ by 1
 					temp = height
 				}
 			}
@@ -52,7 +80,7 @@ class TarjanBridgeFinding<I, E : Edge<I>>(
 			tableHeights[it.idSource] = Pair(minOf(temp, checkAndGetFirst(tableHeights[it.idSource])), height)
 
 			if (checkAndGetFirst(tableHeights[it.idTarget]) > height) {
-				table.add(it)
+				setBridges.add(it)
 			}
 		}
 	}
